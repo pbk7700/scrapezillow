36cff6785d2438dd5ecc20286b466fb670e001c7
greg <greg@MinasArnor.(none)>
2014-09-20 01:19:44 -0700

    initial commit. nothing in here

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = "0.1"
+
+
+setup(
+    name="scratchquillow",
+    author="Gregory Rehm",
+    version=__version__,
+    description="A zillow scraper",
+    packages=find_packages(),
+    package_data={"*": ["*.html"]},
+    install_requires=[
+        "requests"
+    ],
+)
